3462709e68ba8c8af80d6ff3fb83617620b6ef28
Cosmin <cosmin.burdalescu@gmail.com>
2016-03-22 19:58:29 +0000

    added footer

--- footer.php
+++ footer.php
@@ -13,11 +13,15 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'cezar-grigoriu' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'cezar-grigoriu' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'cezar-grigoriu' ), 'cezar-grigoriu', '<a href="http://designify.com" rel="designer">Cosmin Burdalescu</a>' ); ?>
+
+	<footer id="colophon" class="site-footer mdl-mini-footer" role="contentinfo">
+		
+		<div class="site-info mdl-mini-footer__left-section">
+			<div class="mdl-logo"><?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'cezar-grigoriu' ), 'cezar-grigoriu', '<a href="http://designify.com" rel="designer">Cosmin Burdalescu</a>' ); ?></div>
+			<ul class="mdl-mini-footer__link-list">
+				<li><a href="<?php echo esc_url( __( 'https://wordpress.org/', 'cezar-grigoriu' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'cezar-grigoriu' ), 'WordPress' ); ?></a></li>
+			</ul>
+			
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </main>
